c98796795b2517d5393f08bf39900ce80d09a2b8
0xGDSR <gabrieldasilvarosa2023@gmail.com>
2023-03-09 01:48:44 -0300

    base files

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x0231831F0d8FCC25a6C31FB4e29221c470119b8D";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
